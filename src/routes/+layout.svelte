bfa42a6f743e4396e0785d26e3e028f9e748f0f4
Meghashyam1218 <gurrammeghashyam@outlook.com>
2024-02-07 02:38:48 +0530

    cursor

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,16 @@
 <script>
+	import { onMount } from 'svelte';
 	import './styles.css';
+	import { Cursor } from 'curseur';
+	
 </script>
 
 <slot />
+<div class="cursor-element">
+	<Cursor
+		color="green"
+		mixBlendMode="exclusion"
+		size="28"
+		shape= 'circle'
+	/>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,41 @@
-<h1 class="text-3xl font-bold underline">Hello world!</h1>
+<script>
+	import { onMount } from 'svelte';
+	function loader() {
+		const letters = 'ABCDEFGHFIJKLMNOPQRSTUVWXY';
+		let iterations = 0;
+		let interval = null;
+		interval = setInterval(() => {
+			const el = document.getElementById('helloworld');
+			el.innerText = el.innerText
+				.split('')
+				.map((letter, index) => {
+					if (index < iterations) {
+						return el.dataset.value[index];
+					}
+					return letters[Math.floor(Math.random() * 26)];
+				})
+				.join('');
+
+			if (iterations >= el.dataset.value.length) {
+				clearInterval(interval);
+			}
+			iterations += 1 / 2;
+		}, 150);
+	}
+	onMount(() => {
+		loader();
+	});
+</script>
+
+<div class="loader h-[100vh] grid justify-items-center items-center">
+	<!-- svelte-ignore a11y-mouse-events-have-key-events -->
+	<h1 data-value="HELLO WORLD!" id="helloworld" class="text-9xl text-neutral-200 cursor-default">
+		HELLO WORLD!
+	</h1>
+</div>
 
 <style>
-    h1{
-        font-family: Gridular;
-    }
+	.loader h1 {
+		font-family: Gridular;
+	}
 </style>
